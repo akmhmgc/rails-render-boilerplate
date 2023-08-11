d5874d8d8a7ae62aa1b31447c583894b0129b486
akmhmgc <akimuhamaguchi@icloud.com>
2023-08-11 20:37:42 +0900

    fix: add npm install to render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,10 +2,12 @@
 # exit on error
 set -o errexit
 
+npm install
+
 # the current version of Nokogiri ships with incompatible libs
 bundle config --local build.nokogiri --use-system-libraries
 
 bundle install
-bundle exec rake assets:precompile
+bundle exec rake assets:precompile --trace
 bundle exec rake assets:clean
 bundle exec rake db:migrate
